cd3f6008b42c8609311826ca06a077d3800993d2
Manogari Kumaraguru <manogarikumaraguru@gmail.com>
2025-05-01 16:53:02 -0400

    learner

--- src/components/Learner.jsx
+++ src/components/Learner.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-export default function Learner()
+export default function Learner({name,bio})
 {
-    
+    return(
+        <div>
+            <h3>{name}</h3>
+            <p>bio:{bio}</p>
+            <Score/>
+        </div>
+    )
 }
---
+++ src/components/Score.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+export default function Score({name,bio})
+{
+    return(
+        <div>
+            <h3>{date}</h3>
+            <p>score:{score}</p>
+            <Score/>
+        </div>
+    )
+}
